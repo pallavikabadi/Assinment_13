d5233bec9b87774675f97622807e724963a15828
pallavi kabadi <pallavi08jadhav@gmail.com>
2023-03-27 12:59:54 +0530

    added:find Maximum Float Value

--- src/com/bridggelabz/Generics.java
+++ src/com/bridggelabz/Generics.java
@@ -1,27 +1,27 @@
  package com.bridggelabz;
 
 public class Generics<T extends Comparable<T>> {
-    public void FindMaxInt(Integer a, Integer b, Integer c) {
-        int max = a;
-        if ((b.compareTo(max)) > 0) {
-            max = b;
+    public void FindMaxFloat(Float a, Float b, Float c){
+        float max = a;
+        if((b.compareTo(max))>0)
+        {
+            max=b;
         }
-        if ((c.compareTo(max)) > 0) {
-            max = c;
+        if((c.compareTo(max))>0){
+            max=c;
         }
-        System.out.println("Maximum of " + a + "," + b + " and " + c + " is: " + max);
-
+        System.out.println("Maximum of "+a+ ","+b+" and "+c+ " is: "+max);
     }
 
     public static void main(String[] args) {
         Generics genericsProgram = new Generics();
-        Integer a = 10, b = 5, c = 3;
+        Float d = 23.5F ,e=8.3F,f=1.6F;
         // test case 1 :- Given Max Number at 1st Position return the Same Number
-        genericsProgram.FindMaxInt(a, b, c);
+        genericsProgram.FindMaxFloat(d,e,f);
         // test case 2 :- Given Max Number at 2nd Position return the Same Number
-        genericsProgram.FindMaxInt(b, a, c);
+        genericsProgram.FindMaxFloat(e,d,f);
         // test case 3 :- Given Max Number at 3rd Position return the Same Number
-        genericsProgram.FindMaxInt(c, b, a);
+        genericsProgram.FindMaxFloat(d,f,d);
     }
 
 }
