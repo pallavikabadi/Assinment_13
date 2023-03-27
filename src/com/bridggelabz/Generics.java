6d0fe41e55ff6b487c78b308c645f26dca68c38d
pallavi kabadi <pallavi08jadhav@gmail.com>
2023-03-27 12:55:56 +0530

    added:find Maximum Integer Value

--- src/com/bridggelabz/Generics.java
+++ src/com/bridggelabz/Generics.java
@@ -1,34 +1,27 @@
  package com.bridggelabz;
 
 public class Generics<T extends Comparable<T>> {
-    T x,y,z;
-    public Generics(T x, T y, T z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
-    public T FindMax(){
-        return Generics.FindMax(x,y,z);
-    }
-    public static <T extends Comparable<T>>T FindMax(T a, T b, T c){
-        T max=a;
-        if((b.compareTo(max))>0)
-        {
-            max=b;
+    public void FindMaxInt(Integer a, Integer b, Integer c) {
+        int max = a;
+        if ((b.compareTo(max)) > 0) {
+            max = b;
         }
-        if((c.compareTo(max))>0){
-            max=c;
+        if ((c.compareTo(max)) > 0) {
+            max = c;
         }
-        System.out.println("Maximum of "+a+ ", "+b+" and "+c+ " is: "+max);
-        return max;
+        System.out.println("Maximum of " + a + "," + b + " and " + c + " is: " + max);
+
     }
 
     public static void main(String[] args) {
-        Integer a=22,b=4,c=6;
-        Float d = 12.3F ,e=5.9F,f=2.3F;
-        String g="Anjali",h="Rawke",i="Zain";
-        new Generics<Integer>(a,b,c).FindMax();
-        new Generics<Float>(d,e,f).FindMax();
-        new Generics<String>(g,h,i).FindMax();
+        Generics genericsProgram = new Generics();
+        Integer a = 10, b = 5, c = 3;
+        // test case 1 :- Given Max Number at 1st Position return the Same Number
+        genericsProgram.FindMaxInt(a, b, c);
+        // test case 2 :- Given Max Number at 2nd Position return the Same Number
+        genericsProgram.FindMaxInt(b, a, c);
+        // test case 3 :- Given Max Number at 3rd Position return the Same Number
+        genericsProgram.FindMaxInt(c, b, a);
     }
+
 }
